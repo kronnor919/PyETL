17cd60079b515e90e2f58cd8e0bdd52d9ffa21ad
kronnor919 <segundacuenta7364@gmail.com>
2025-06-25 13:56:32 -0400

    feat(logger): add Singleton structure

---
+++ src/core/logger.py
@@ -0,0 +1,15 @@
+from threading import Lock
+
+class Logger:
+    _instance = None
+    _lock = Lock()
+
+    def __new__(cls):
+        if cls._instance is None:
+            with cls._lock:
+                if cls._instance is None:
+                    cls._instance = super().__new__(cls)
+                    cls._instance._initialize()
+    
+    def _initialize(self):
+        pass
